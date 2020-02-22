7ee2814f63eed5b85a116a028268064987dbb023
jlschiff2 <jenn.schiffman@kellogg.northwestern.edu>
2020-02-22 23:02:44 +0000

    news app - attempt 1

--- app.rb
+++ app.rb
@@ -1,18 +1,70 @@
 require "sinatra"
 require "sinatra/reloader"
 require "geocoder"
-require "forecast_io"
-require "httparty"
+#require "forecast_io"
+#require "httparty"
 def view(template); erb template.to_sym; end
-before { puts "Parameters: #{params}" }                                     
+before { puts "Parameters: #{params}" }  
 
 # enter your Dark Sky API key here
-ForecastIO.api_key = "YOUR-API-KEY"
+# ForecastIO.api_key = "6db4f62deb242d6ae74da3d760af00d6"
 
 get "/" do
-  # show a view that asks for the location
+    view "app"
 end
 
 get "/news" do
-  # do everything else
+    # turn city into lat-long coordinates
+    results = Geocoder.search(params["q"])
+    lat_long = results.first.coordinates #=> [lat, long]
+    @lat = "#{lat_long[0]}"
+    @long = "#{lat_long[1]}"
+    puts "#{@lat} #{@long}"
+    
+    # send lat-long to Dark Sky to retrieve weather
+    forecast = ForecastIO.forecast("#{lat}", "#{long}").to_hash
+    current_temperature = forecast["currently"]["temperature"]
+    conditions = forecast["currently"]["summary"]
+
+    # display current weather
+    puts "In #{q}, it is currently #{current_temperature} and #{conditions}."
+
+    # display weather forecast
+    for day in forecast["daily"]["data"]
+    puts "A high temperature of #{day["temperatureHigh"]} and #{day["summary"]}"
+    end
+
+    # display national headlines
+    @location = params["q"]
+    @geocoder_results = Geocoder.search(@location)
+    @lat_long = @geocoder_results.first.coordinates # => [lat, long] array
+    @url = "https://newsapi.org/v2/top-headlines?country=us&apiKey=4bd84eb679e949d597951ba41ca8d754"
+    news = HTTParty.get(@url).parsed_response.to_hash    
+    pp news
 end
+
+
+# url = "https://newsapi.org/v2/top-headlines?country=us&apiKey=4bd84eb679e949d597951ba41ca8d754"
+# news = HTTParty.get(url).parsed_response.to_hash
+# news is now a Hash you can pretty print (pp) and parse for your output
+
+# results = Geocoder.search("Evanston, IL")
+# results.first.coordinates #=> [42.0574063,-87.6722787]
+
+# get "/news" do
+  # do everything else
+  #results = Geocoder.search(params["q"])
+   # @lat_long = results.first.coordinates => [lat,long]
+    #"#{@lat_long[0]} #{@lat_long[1]}"
+
+    #current_temp = forecast["currently"]["temperature"]
+    #current_condition = forecast["currently"]["summary"]
+        #puts "In #{forecast["timezone"]}, it is currently #{current_temp} degrees and #{current_condition}"
+
+# a loop gets built on an array, not a hash!
+# variable made for what the array is, then you can use that to locate the data you need.
+
+    #for day in forecast["daily"]["data"]
+    #puts "A high temperature of #{day["temperatureHigh"]} and #{day["summary"]}"
+    #end
+#end
